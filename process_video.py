458b34881f5bb07759b877413af6fb80012451dc
Anand <anand13394@gmail.com>
2020-09-19 15:16:53 +0530

    added scorer + result path

--- process_video.py
+++ process_video.py
@@ -10,6 +10,7 @@ argparser.add_argument('-i', dest="video_path",help="path to the video file", re
 argparser.add_argument('-s', dest="save_csv",help="save csv results", default=True)
 args = argparser.parse_args()
 
-_, extension_ = os.path.splittext( args.video_path)
+path, extension_ = os.path.splitext( args.video_path)
 
-deeplabcut.analyze_videos( args.config, [ args.video_path ], videotype= extension_, save_as_csv= args.save_csv)
+DLCScorer = deeplabcut.analyze_videos( args.config, [ args.video_path ], videotype= extension_, save_as_csv= args.save_csv)
+print("Result path : ",  path + DLCScorer + '.csv')
