19f50a740e19970661a97b943952a515d1f16a86
Anand <anand13394@gmail.com>
2020-09-23 18:11:12 +0530

    plot angle json

---
+++ plot_joint_angle_json.py
@@ -0,0 +1,24 @@
+import pdb 
+
+import argparse as arg
+
+import json
+
+from matplotlib import pyplot as plt
+
+# Input Processing
+argparser = arg.ArgumentParser(description="Process angle json")
+argparser.add_argument('-i', dest="json_path",help="path to the angle json file", required=True)
+args = argparser.parse_args()
+
+
+with open( args.json_path ) as file:
+  angle_data = json.load( file)
+
+fig, ax = plt.subplots()
+for key in angle_data.keys():
+	ax.plot( angle_data[key], '-*',label=key)
+
+legend = ax.legend(loc='lower center', fontsize='small')
+plt.title( 'Angles')
+plt.show()
