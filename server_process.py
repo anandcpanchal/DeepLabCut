06730ff4c8318fe536fad6c4f25b5599715e9486
Anand Panchal <anand13394@gmail.com>
2020-11-28 15:16:32 +0530

    indentation fix

--- server_process.py
+++ server_process.py
@@ -47,22 +47,22 @@ class ServerProcess:
 
 if __name__ == "__main__":
 	# Input Processing
-    argparser = arg.ArgumentParser(description="Process video repetition count with DeepLabCut")
-    argparser.add_argument('-c', dest="config", default="./inference_config.json", help="config_json_path", required=True)
-    args = argparser.parse_args()
+	argparser = arg.ArgumentParser(description="Process video repetition count with DeepLabCut")
+	argparser.add_argument('-c', dest="config", default="./inference_config.json", help="config_json_path", required=True)
+	args = argparser.parse_args()
 
-    wait_counter = 0
+	wait_counter = 0
 
-    inference_config_file = open( args.config )
-    config = json.load( inference_config_file )
-    db_path = config['db_path']
-    process = ServerProcess( db_path, 'RECORD')
-    while True:
-    	if process.debug: print("Fetching record from db")
-    	record = process.get_record_for_processing()	
-    	if record:
-    		wait_counter = 0
-    		metadata = record['metadata']
+	inference_config_file = open( args.config )
+	config = json.load( inference_config_file )
+	db_path = config['db_path']
+	process = ServerProcess( db_path, 'RECORD')
+	while True:
+		if process.debug: print("Fetching record from db")
+		record = process.get_record_for_processing()	
+		if record:
+			wait_counter = 0
+			metadata = record['metadata']
 			try:
 				temp_video_storage_path = './input/'
 				if not os.path.exists( temp_video_storage_path):
@@ -75,21 +75,21 @@ if __name__ == "__main__":
 				print("Error fetching content")
 				exit()
 
-    		output = process_video( config['inference_config'], record['video'], record['metadata'], config['output_video_boolean_flag'])
-    		if process.debug: 
-    			for key in output.keys():
-    				print( key, " : ", output[key])
-    		if output:
-    			metadata = json.loads( metadata.replace('\'','\"') )# convert metadata string to dictionary
-    			process.db.update_processed_flag( record["id"], 1)
-    			record["processed"] = 1
-    			if process.debug: print("Processing : ", record)
-    			process.db.update_result( record['id'], output['counter_data'][ metadata['primary_organ']], output['counter_data'][ metadata['secondary_organ']])
-    			process.db.update_published_flag( record["id"], 1)
-    			record["published"] = 1
-    			if process.debug: print("Published : ", record)
-    	else:
-    		wait_counter += 1
-    		if process.debug: print(str( wait_counter) ," >> No pending record found, sleeping for 10 sec")    		
-    		process.wait(10)
-    		pass
+			output = process_video( config['inference_config'], record['video'], record['metadata'], config['output_video_boolean_flag'])
+			if process.debug: 
+				for key in output.keys():
+					print( key, " : ", output[key])
+			if output:
+				metadata = json.loads( metadata.replace('\'','\"') )# convert metadata string to dictionary
+				process.db.update_processed_flag( record["id"], 1)
+				record["processed"] = 1
+				if process.debug: print("Processing : ", record)
+				process.db.update_result( record['id'], output['counter_data'][ metadata['primary_organ']], output['counter_data'][ metadata['secondary_organ']])
+				process.db.update_published_flag( record["id"], 1)
+				record["published"] = 1
+				if process.debug: print("Published : ", record)
+		else:
+			wait_counter += 1
+			if process.debug: print(str( wait_counter) ," >> No pending record found, sleeping for 10 sec")    		
+			process.wait(10)
+			pass
